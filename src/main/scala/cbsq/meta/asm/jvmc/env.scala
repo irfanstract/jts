2e84fed54e471945f262d501f9d44d59c42d378a
me <meh@meh>
2023-05-17 10:39:32 +0700

    `src\main\meta\asm\jvmc` - apply these changes

---
+++ src/main/scala/cbsq/meta/asm/jvmc/env.scala
@@ -0,0 +1,49 @@
+package cbsq.meta.asm.jvmc
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export cbsq.meta.asm.jvm.ow
+
+
+
+
+export cbsq.meta.asm.jvm.{Esig}
+
+export cbsq.meta.asm.jvm.{visitXSig, visitXSigReturnType}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
