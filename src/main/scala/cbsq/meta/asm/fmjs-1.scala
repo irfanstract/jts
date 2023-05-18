8bc6b33f923f03cdd5b3c5cda94af966cf7ffa3c
me <meh@meh>
2023-05-19 01:27:41 +0700

    Revert "apply these (incomplete) changes"
    This reverts commit 93795c3cb652507c1ead2e6f785106a6c01c6686.

--- src/main/scala/cbsq/meta/asm/fmjs-1.scala
+++ src/main/scala/cbsq/meta/asm/fmjs-1.scala
@@ -280,18 +280,17 @@ def wsnImpl() = {
                      o.println()
                   }
                   val instrS = {
-                     // instr.toJsBlockLevelStmt()
-                     // .replaceFirst({
-                     //    import util.matching.Regex.{quoteReplacement, quote}
-                     //    ";\\s*" + (quote("//") + "[\\S\\s]*?" ).prependedAll("(?:").++(")").++("??") + "\\z"
-                     // }, "")
-                     // // .appendedAll(" ;")
-                     // // .prependedAll("\n")
-                     // // .prependedAll({
-                     // //    "const " + (operandsForIndex(instrOrdinal) + "$im1") + " = " + (operandsForIndex(instrOrdinal + -1) + "$om1") + " ;"
-                     // // } )
-                     // // .replaceFirst("[\\S\\s]*", "{ $0 }")
-                     ??? : String
+                     instr.toJsBlockLevelStmt()
+                     .replaceFirst({
+                        import util.matching.Regex.{quoteReplacement, quote}
+                        ";\\s*" + (quote("//") + "[\\S\\s]*?" ).prependedAll("(?:").++(")").++("??") + "\\z"
+                     }, "")
+                     // .appendedAll(" ;")
+                     // .prependedAll("\n")
+                     // .prependedAll({
+                     //    "const " + (operandsForIndex(instrOrdinal) + "$im1") + " = " + (operandsForIndex(instrOrdinal + -1) + "$om1") + " ;"
+                     // } )
+                     // .replaceFirst("[\\S\\s]*", "{ $0 }")
                   }
                   o.println(s"${instrS.indent(2 * 2).dropRight(1) } ;" )
                }
