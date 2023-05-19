8b1e087ac7bd349415f454394919536f83e3b268
me <meh@meh>
2023-05-19 11:25:43 +0700

    `src\main\meta\asm` - `utilityImplicits` - add `unfolding`

--- src/main/scala/cbsq/meta/asm/env.scala
+++ src/main/scala/cbsq/meta/asm/env.scala
@@ -23,6 +23,21 @@ object utilityImplicits {
 
    }
 
+   extension [CC[A] <: collection.SeqOps[A, CC, CC[A]], E](s: CC[E]) {
+
+      def unfolding[Count](v0: Count)(f: (Count, E) => Count): CC[(E, Count)] = {
+         val s1 = s.to(LazyList)
+         Iterator.unfold[(E, Count), (Count, LazyList[E])]((v0, s1))({
+            case (state0, presentlyItem +: remainingItems) =>
+               val state1 = f(state0, presentlyItem)
+               Some(((presentlyItem, state1): (E, Count), (state1, remainingItems)))
+         })
+         .to(s.iterableFactory )
+         // ???
+      }
+      
+   }
+
 }
 
 export utilityImplicits._
