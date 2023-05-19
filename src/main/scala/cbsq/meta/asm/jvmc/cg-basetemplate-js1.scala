200bb3022ff2a86a141f75b78ddbe2160ed7dd8f
me <meh@meh>
2023-05-19 18:31:14 +0700

    `src\main\meta\asm\jvmc\cg-basetemplate-js.scala` - update `toXJsString21`

--- src/main/scala/cbsq/meta/asm/jvmc/cg-basetemplate-js1.scala
+++ src/main/scala/cbsq/meta/asm/jvmc/cg-basetemplate-js1.scala
@@ -378,6 +378,99 @@ class wsnImplCtx1() {
 
       }
 
+      // TODO
+      def toXJsString21(
+         opc: Int ,
+         odst: MethodDescriptorImpl1 ,
+         opdState0: Jblt.OpdState[FqnStronumericPair[?] ],
+         
+         documentOriginalSrc: Boolean ,
+         async: Boolean ,
+
+      )(using InOpdCtx) : Jblt.OfStorageType[FqnStronumericPair[?] ] = {
+                     // import scala.language.unsafeNulls
+
+                     import scala.jdk.CollectionConverters.*
+                     import org.objectweb.asm
+                     import cbsq.meta.asm.jvm.opcodeNameTable
+
+                     val opcodeTranslitAnalysis = (
+                        analyseMethodInvocOpcTranslitImpl(MethodInvocOpcode.check(opc), odst, opdState0)
+                     )
+                     import opcodeTranslitAnalysis.*
+
+                     val optionalReturnValueCompiledVarName = (
+                        (opdStackState2.fromLeftRightwards.toSet diff opdStackState1.fromLeftRightwards.toSet )
+                        .lastOption
+                        .map(_.toSingleWordNameString() )
+                     )
+
+                     import util.matching.Regex.{quote, quoteReplacement}
+
+                     val compiledCode = (
+                        odst
+                        .toJsMethodName()
+                        .++(/* async */ (
+                           // TODO
+                           if async then "Asynchronously"
+                           else ""
+                        )).nn
+                        .++(s"($nonReceiverArgsSpread )")
+                        .prependedAll(".")
+                        .prependedAll(receiverAlias )
+                        .++((
+                           if documentOriginalSrc then s" /* $argdsc */"
+                           else "" 
+                        ))
+                        .replaceFirst("[\\S\\s]*", /* async */ (
+                           if async then "await $0"
+                           else "$0"
+                        )).nn
+                        .replaceFirst("[\\S\\s]*", (
+                           quoteReplacement(optionalReturnValueCompiledVarName match {
+
+                              case Some(returnValCompiledVarName) =>
+                                 s"const ${returnValCompiledVarName } = "
+
+                              case None => 
+                                 "/* V */ "
+
+                           }) + "$0" + " ;"
+                        )).nn
+                        
+                        match {
+                        case s =>
+                           s
+                        }
+                     ) 
+                     
+                     new Jblt {
+
+                        override val transliteratedForm = compiledCode
+                        
+                        override
+                        val resultingOpdState: Jblt.OpdState[FqnStronumericPair[?] ] = {
+                           opdState0
+                           .copy(
+                              opdStack = {
+                                 opdStackState2
+                              } ,
+                              lastItemgenState = {
+                                 (opdStackState0.fromLeftRightwards ++ opdStackState2.fromLeftRightwards)
+                                 .maxBy({
+                                    case (_, i) =>
+                                       i + (
+                                          1
+                                       )
+                                 })
+                              } ,
+                           )
+                        }
+   
+                     }
+                     
+      }
+
    }
    /* avoid using wildcard imports as there are other stuffs */ 
    export eRpkImpl.getBaseTemplate
